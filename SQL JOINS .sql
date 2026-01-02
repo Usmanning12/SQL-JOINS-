9e9f2e83d3e6461a2f17526a5907621c32ada117
Darron Manning <darronmanning@yahoo.com>
2026-01-01 19:38:33 -0600

    SQL Joins

--- SQL JOINS .sql
+++ SQL JOINS .sql
@@ -24,9 +24,22 @@ Inner Join Sales s
 ON e.EmployeeId = s.EmployeeId
 
 /* joins: find the name of the department, and the name of the category for Appliances and Games */
+SELECT d.Name as DepartmentName, c.Name as CategoryName
+From Categories c 
+Inner Join Departments d 
+On c.departmentID = d.departmentID
+WHERE c.name IN('Appliances', 'Games'); 
+
 
 /* joins: find the product name, total # sold, and total price sold,
  for Eagles: Hotel California --You may need to use SUM() */
+ SELECT p.name as ProductName, 
+SUM (s.quantity) as TotalNumber, 
+SUM *(s.quantity * p.price) AS TotalPrice
+ From Sales s
+ Inner Join Products p
+ On s.productID = p.productID
+ 
 
 /* joins: find Product name, reviewer name, rating, and comment on the Visio TV. (only return for the lowest rating!) */
 -- ------------------------------------------ Extra - May be difficult
@@ -36,4 +49,24 @@ This query should return:
 -  the employee's first and last name
 -  the name of each product
 -  and how many of that product they sold */
+SELECT  p.name as ProductName,
+ rv.firstName as ReviewerFirstName,
+ rv.lastName as ReviewerLastName,
+ r.Rating, 
+ r.Comment 
+FROM Reviews r
+Inner Join Products p
+On r.productID = p.productID
+Inner Join Reviewers rv 
+ON r.reviewerID = rv.ReviewerID
+Where p.name = 'Visio TV'
+AND r.rating = (
+SELECT MIN (r2.Rating)
+From Reviews r2 
+Inner JOIN Products p2
+	ON r2.ProductID = p2.ProductId
+    Where p2.Name = 'Visio TV'
+);
+
+
 
