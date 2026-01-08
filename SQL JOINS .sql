1add43c16fa319c12d263344f5ec3d80eb0997d2
Darron Manning <darronmanning@yahoo.com>
2026-01-08 09:37:06 -0600

    Updated SQL Joins

--- SQL JOINS .sql
+++ SQL JOINS .sql
@@ -18,17 +18,17 @@ Where r.rating = 5;
 
  
 /* joins: find the employee with the most total quantity sold.  use the sum() function and group by */
-Select e.name as EmployeeName, s.Quantity as SalesQuantity
-From Employees e 
-Inner Join Sales s 
-ON e.EmployeeId = s.EmployeeId
+Select employees.FirstName, employees.LastName, SUM(sales.Quantity) AS total
+From sales
+Inner Join employees ON employees.EmployeeID = sales.EmployeeID
+GROUP BY employees.EmployeeID, employees.FirstName, employees.LastName
+Order BY Total DESC LIMIT 1;
 
 /* joins: find the name of the department, and the name of the category for Appliances and Games */
-SELECT d.Name as DepartmentName, c.Name as CategoryName
-From Categories c 
-Inner Join Departments d 
-On c.departmentID = d.departmentID
-WHERE c.name IN('Appliances', 'Games'); 
+SELECT departments.Name, categories.Name
+From departments
+Inner Join categories On categories.DepartmentID = departments.DepartmentID 
+WHERE categories.Name = 'Appliances' OR categories.Name = 'Games'; 
 
 
 /* joins: find the product name, total # sold, and total price sold,
@@ -38,10 +38,17 @@ SUM (s.quantity) as TotalNumber,
 SUM *(s.quantity * p.price) AS TotalPrice
  From Sales s
  Inner Join Products p
- On s.productID = p.productID
+ On s.productID = p.productID;
  
 
 /* joins: find Product name, reviewer name, rating, and comment on the Visio TV. (only return for the lowest rating!) */
+SELECT products.Name, products.ProductID FROM products ORDER BY products.Name DESC; /* got product ID */
+SELECT products.Name, reviews.Reviewer, reviews.Rating, reviews.Comment 
+FROM products
+Inner Join reviews ON reviews.ProductID = products.ProductID
+Where products.ProductID = 857 
+	AND reviews.Rating = (SELECT MIN(Rating) FROM reviews WHERE ProductID = 857);
+
 -- ------------------------------------------ Extra - May be difficult
 /* Your goal is to write a query that serves as an employee sales report.
 This query should return:
@@ -49,24 +56,7 @@ This query should return:
 -  the employee's first and last name
 -  the name of each product
 -  and how many of that product they sold */
-SELECT  p.name as ProductName,
- rv.firstName as ReviewerFirstName,
- rv.lastName as ReviewerLastName,
- r.Rating, 
- r.Comment 
-FROM Reviews r
-Inner Join Products p
-On r.productID = p.productID
-Inner Join Reviewers rv 
-ON r.reviewerID = rv.ReviewerID
-Where p.name = 'Visio TV'
-AND r.rating = (
-SELECT MIN (r2.Rating)
-From Reviews r2 
-Inner JOIN Products p2
-	ON r2.ProductID = p2.ProductId
-    Where p2.Name = 'Visio TV'
-);
+
 
 
 
